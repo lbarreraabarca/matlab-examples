57a9e6c4df7ab9988e1d6ca7c462307ec11842cd
root <root@LAPTOP-ES3L2JV9.localdomain>
2019-05-04 13:26:34 -0400

    Solve problem 1

--- example1/ejercicio1.m
+++ example1/ejercicio1.m
@@ -2,75 +2,95 @@
 clear all;
 
 %Se solicitan los datos para la primera funcion
-func=' f(x) : '
-validate=1
-[f, f_inf, f_sup, itmax ]=getData( func, validate )
-%Se solicitan los datos para la segunda funcion
-func=' g(x) : '
-validate=0
-g=getData( func, validate )
+func=' f(x) : ';
+[f, f_inf, f_sup, itmax ]=getData( func );
 
-y=biseccion2(f,g, f_inf, f_sup, itmax )
+[it, values ]=biseccion2(f, f_inf, f_sup, itmax );
 
-function [f, f_inf, f_sup, itmax ] = getData( func, validate )
+plot(it, values )
+
+function [f, f_inf, f_sup, itmax ] = getData( func )
     %Variables genericas de texto
-    ingrese='Ingrese '
-    lim_inf= ' limite inferior de '
-    lim_sup= ' limite superior de '
-    itmax_txt= ' itmax : '
+    ingrese='Ingrese ';
+    lim_inf= ' limite inferior de ';
+    lim_sup= ' limite superior de ';
+    itmax_txt= ' itmax : ';
 
-    %Texto para solicitar engresar la funcion 
-    texto_func=strcat(ingrese, func)
+    %Texto para solicitar ingresar la funcion 
+    texto_func=strcat(ingrese, func);
 
-    f_str=input(texto_func)
-    f=inline(f_str, 'x')
-
-    %Valida que solo pida los limites una sola vez
-    if validate == 1
-        %Texto para solicitar el limite inferior
-        texto_inf=strcat(ingrese , lim_inf)
-        texto_inf=strcat(texto_inf , func)
-
-        %Texto para solicitar el limite superior
-        texto_sup=strcat(ingrese , lim_sup)
-        texto_sup=strcat(texto_sup , func)
-        
-        %Texto para solicitar cantidad de iteraciones
-        texto_itmax=strcat(ingrese, itmax_txt)
-        
-        %Se asigna la entrada por teclado del limite inferior
-        f_inf=input(texto_inf)
-        %Se asigna la entrada por teclado del limite superior
-        f_sup=input(texto_sup)
-        %Se asigna la entrada por teclado del itmax
-        itmax=input(texto_itmax)
-        
-    else
-        %Se asigna 0 dado que no se utiliza
-        f_inf=0
-        f_sup=0
-    end
-end
-
-
-function y = biseccion2( f, g, a , b, itmax )
-    %Params:
-    % f corresponde a la funcion f(x)
-    % g corresponde a la funcion g(x)
-    % a corresponde al limite inferior
-    % b corresponde al limite superior
-    % itmax corresponde a la cantidad de iteraciones
+    f=input(texto_func, 's');
     
-    c = b - (f(b)*(a-b))/(f(a)-f(b));
-    if (g(a)*g(b)) <= 0
-        while ((g(c)~=0) && (abs(b-a)>itmax))
-            if (g(c)*g(b)) > 0
-                a=c
-            else
-                b=c
-            end
-            c=(a+b)/2
-        end 
-    end
-    y=c
+    %Texto para solicitar el limite inferior
+    texto_inf=strcat(ingrese , lim_inf);
+    texto_inf=strcat(texto_inf , func);
+
+    %Texto para solicitar el limite superior
+    texto_sup=strcat(ingrese , lim_sup);
+    texto_sup=strcat(texto_sup , func);
+        
+    %Texto para solicitar cantidad de iteraciones
+    texto_itmax=strcat(ingrese, itmax_txt);
+        
+    %Se asigna la entrada por teclado del limite inferior
+    f_inf=input(texto_inf);
+    %Se asigna la entrada por teclado del limite superior
+    f_sup=input(texto_sup);
+    %Se asigna la entrada por teclado del itmax
+    itmax=input(texto_itmax);
 end
+
+function [ it, values ] = biseccion2( f, a , b, error, itmax )
+      disp('Metodo de biseccion');
+      it=[];
+      values=[];
+      %Margen de tolerancia al error
+      error=0.001;
+      %Se carga la funcion f(x)
+      f=inline(f);
+      %Variable para controlar la cantidad de iteraciones
+      i=1;
+      ea(1)=100;
+      %Se valida que existan raices 
+      if f(a)*f(b) < 0
+        %Xa evaluada en 1 corresponde al limite inferior
+        xa(1)=a;
+        %Xb evaluada en 1 corresponde al limite superior
+        xb(1)=b;
+        %Xr corresponde al punto medio de evaluar xa y xb en 1
+        xr(1)=(xa(1)+xb(1))/2;
+        %Impresiones de pantalla para el primer resultado
+        fprintf('It \t r \n');
+        fprintf('%2d %11.7f \n',i,xr(i));
+        it(i)=i;
+        values(i)=xr(i);
+        %Bucle que permite iterar la solución
+        while abs(ea(i)) >= error,
+            if f(xa(i))*f(xr(i))<0
+                xa(i+1)=xa(i);
+                xb(i+1)=xr(i);
+            end
+            if f(xa(i))*f(xr(i))>0
+                xa(i+1)=xr(i);
+                xb(i+1)=xb(i);
+            end
+            %Xr corresponde al punto medio de evaluar xa y xb en i+1
+            xr(i+1)=(xa(i+1)+xb(i+1))/2;
+            ea(i+1)=abs((xr(i+1)-xr(i))/(xr(i+1))*100);
+            %Impresion de pantalla de la ene-sima iteracion
+            fprintf('%2d \t %11.7f \n',...
+            i+1,xr(i+1));
+            it(i+1)=i+1;
+            values(i+1)=xr(i+1);
+            % Se incremanta la variable iterante
+            i=i+1;
+        end
+      else
+        fprintf('NO EXISTE UNA RAIZ EN ESTE INTERVALO\n');
+      end
+end
+
+
+
+
+
--- example2/ejercicio2.m
+++ example2/ejercicio2.m
@@ -1,60 +1,107 @@
 clear;
-
 %Gravedad
-g = 9.8
+g = 9.8;
 %Llamada a la funcion que solicita los parametros de entrada.
-% [ dens_liq, dens_par, mu, dp ] = getData()
+[ dens_liq, dens_par, mu, dp ] = getData();
 
 % Factor de friccion
-% cd = ( 4 * g * dp * ( dens_par - dens_liq ) ) / ( 3 * dens_liq )
+cd = ( 4 * g * dp * ( dens_par - dens_liq ) ) / ( 3 * dens_liq );
 
-output = getReynoldsNumber( ) 
+%Reynolds y cd 1era funcion 
+cd1_str='24/x';
+re1_min=-inf;
+re1_max=0.1;
+[ cd1_min, cd1_max ] = getRangeCd( cd1_str, re1_min, re1_max );
 
-function output = getReynoldsNumber( )
-    
-    syms y x
-    %1era ecuacion funcion por rama
-    eq1 = y == ( 24 / x )
-    inv_eq1 = solve( eq1, x )
-    
-    %2da ecuacion funcion por rama
-    eq2 = y == (24/x)*(1 + 0.14*x^(0.7))
-    inv_eq2 = solve( eq2, x )
-    
+%Reynolds y cd 2da funcion
+cd2_str='(24/x)*(1+0.14*x^(0.7))';
+re2_min=0.1;
+re2_max=1000;
+[ cd2_min, cd2_max ] = getRangeCd( cd2_str, re2_min, re2_max );
+
+%Reynolds y cd 3era funcion
+re3_min=1000;
+re3_max=350000;
+cd3_min=0.44;
+cd3_max=0.44;
+
+%Reynolds y cd 4ta funcion
+cd4_str='-19-(80000/x)';
+re4_min=350000;
+re4_max=inf;
+[ cd4_min, cd4_max ] = getRangeCd( cd4_str, re4_min, re4_max );
+
+
+%Calcular el numero de reynolds
+re = getReynoldsNumber( cd, cd1_min, cd1_max, cd2_min, cd2_max, cd3_min, cd3_max, cd4_min, cd4_max, re1_min, re1_max, re2_min, re2_max, re3_min, re3_max, re4_min, re4_max );
+
+%Funcion para obtener las cotas inferiores y superiores.
+function [ min , max ] = getRangeCd( f_str, x_min, x_max )
+    f=inline(f_str, 'x');
+    min=f(x_min);
+    max=f(x_max);
+    if ( max < min )
+        aux=max;
+        max=min;
+        min=aux;
+    end
+end
+
+function reynolds_value = getReynoldsNumber( cd, cd1_min, cd1_max, cd2_min, cd2_max, cd3_min, cd3_max, cd4_min, cd4_max, re1_min, re1_max, re2_min, re2_max, re3_min, re3_max, re4_min, re4_max )
+    syms y x;
+    if ( cd >= cd1_min ) && ( cd <= cd1_max )
+        eq = y == ( 24 / x );
+    elseif ( cd >= cd2_min ) && ( cd <= cd2_max )
+        eq = y == (24/x)*(1+0.14*x^(0.7));
+    elseif ( cd >= cd3_min ) && ( cd <= cd3_max )
+        eq = y == 0.44;
+    elseif ( cd >= cd4_min ) && ( cd <= cd4_max )
+        eq = y == -19 -(80000/x);
+    end
+    reynolds_value=getInverse( eq, cd );   
+end
+
+function rv = getInverse( eq, cd )
+    %Representacion de los simbolos para la ecuacion
+    syms y x;
+    %Obtiene la funcion inversa de la ecuacion
+    inv_eq=solve( eq, x);
+    inverse_function=inline(inv_eq);
+    %Se evalua la ecuacion de numero de reynolds a partir del valor de cd
+    rv=inverse_function(cd);
 end
 
 
 function [ dens_liq, dens_par, mu, dp ] = getData( )
     %Variables genericas de texto
-    ingrese='Ingrese '
-    txt_dens_liq= ' densidad del liquido : '
-    txt_dens_par= ' densidad de la particula : '
-    txt_mu= ' viscosidad del liquido : '
-    txt_dp= ' diametro de la particula : '
+    ingrese='Ingrese ';
+    txt_dens_liq= ' densidad del liquido : ';
+    txt_dens_par= ' densidad de la particula : ';
+    txt_mu= ' viscosidad del liquido : ';
+    txt_dp= ' diametro de la particula : ';
     
     %Texto para solicitar ingresar la densidad del liquido
-    texto_dens_liq=strcat(ingrese, txt_dens_liq)
+    texto_dens_liq=strcat(ingrese, txt_dens_liq);
     
     %Texto para solicitar ingresar la densidad de la particula
-    texto_dens_par=strcat(ingrese, txt_dens_par)
+    texto_dens_par=strcat(ingrese, txt_dens_par);
     
     %Texto para solicitar ingresar la viscosidad del liquido
-    texto_mu=strcat(ingrese, txt_mu)
+    texto_mu=strcat(ingrese, txt_mu);
     
     %Texto para solicitar ingresar el diametro de la particula
-    texto_dp=strcat(ingrese, txt_dp)
+    texto_dp=strcat(ingrese, txt_dp);
     
     %Se asigna la entrada por teclado de la densidad del liquido
-    dens_liq=input(texto_dens_liq)
+    dens_liq=input(texto_dens_liq);
     
     %Se asigna la entrada por teclado de la densidad del liquido
-    dens_par=input(texto_dens_par)
+    dens_par=input(texto_dens_par);
     
     %Se asigna la entrada por teclado de la viscosidad del liquido
-    mu=input(texto_mu)
+    mu=input(texto_mu);
     
     %Se asigna la entrada por teclado del diametro de la particula
-    dp=input(texto_dp)
+    dp=input(texto_dp);
     
 end 
-
